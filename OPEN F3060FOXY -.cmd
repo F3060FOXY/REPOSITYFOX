34ceb1105250d7e217dfb83b5c924b1cc2cd9ca0
F3060FOXY <80268623+F3060FOXY@users.noreply.github.com>
2021-08-09 14:26:05 -0300

    Add files via upload

---
+++ OPEN F3060FOXY -.cmd
@@ -0,0 +1,90 @@
+@echo off
+%~d0
+CD %~dp0
+echo ==================================================================================================================
+echo F3060FOXYGAMES intall cmd  e bat
+
+echo                                   (obrigado por baixar em nosso canal e em nosso site)
+echo.
+echo Microsoft Visual C++ All-In-One Runtimes by F3060FOXY
+echo https://sites.google.com/view/f3060foxygames-oficial/f3060foxygames-pc-low-exe-ver-nova-central-de-download
+echo                                                 by canal F3060FOXY 
+echo                                              web site F3060FOXYGAMES
+echo                                https://www.youtube.com/c/F3060FOXY nosso canal oficial 
+echo                                https://discord.com/invite/YNGSvmv nosso descord oficial 
+echo https://sites.google.com/view/f3060foxygames-oficial/f3060foxygames-pc-low-exe-ver-nova-central-de-download
+echo                                                      F3060FOXY
+
+echo                                                (F3060FOXY©2015-2025)
+
+echo. 
+
+
+echo                                                   F3060FOXY.REMIX
+
+echo                                                      F3060FOXY
+
+echo                                                   F3060FOXYGAMES
+
+echo                                                   F3060FOXYSTUDIO
+
+echo                                                    F3060FOXYCORP
+
+echo                                                 F3060FOXY.copyright
+
+echo                                                     F3060FOXY©
+
+echo.
+
+
+echo                                          This software is provided F3060FOXY
+
+echo.
+
+
+echo                                       This software is provided by F3060FOXYgames
+
+
+
+
+echo                                                 F3060FOXY.copyright
+
+echo =====================================================================================================================
+
+echo.
+
+echo Installing runtime packages... (nao e todos que esta instalando)
+echo                                           F3060FOXY 
+
+
+echo=============================================================
+
+echo by canal F3060FOXY 
+echo web site F3060FOXYGAMES
+echo https://www.youtube.com/c/F3060FOXY nosso canal oficial 
+echo https://discord.com/invite/YNGSvmv nosso descord oficial 
+echo https://sites.google.com/view/f3060foxygames-oficial/f3060foxygames-pc-low-exe-ver-nova-central-de-download
+
+echo  completed successfully by F3060FOXY
+
+
+echo Installation completed successfully by canal F3060FOXY
+
+echo                                   (obrigado por baixar em nosso canal e em nosso site)
+
+start https://sites.google.com/view/f3060foxygames-oficial/f3060foxygames-pc-low-exe-ver-nova-central-de-download
+
+start https://discord.com/invite/YNGSvmv
+
+start https://www.youtube.com/c/F3060FOXY
+
+echo                                                    F3060FOXY.copyright
+
+goto END
+
+:END
+
+echo.
+echo Installation completed successfully by canal F3060FOXY
+
+exit
